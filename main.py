2fcbe252f412063555781454348a76f139ba1a49
Jesse <jesseb727@gmail.com>
2020-07-02 20:00:09 +0200

    Add decimal support

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from tkinter import Tk, ttk, StringVar
-import tkinter.scrolledtext as st
-import tkinter as tk
+# Normal Imports
 import re
+import tkinter as tk
+import tkinter.scrolledtext as st
 
-int_pattern = re.compile(r"^\d")
+# From Imports
+from collections import namedtuple
+from tkinter import StringVar, Tk, ttk
+
+
+int_pattern = re.compile(r"^\d+$")
+intfloat_pattern = re.compile(r"(^\d+)(\.)?(\d+)?$")
 
 
 def get_first_int(val, default):
@@ -20,6 +26,85 @@ otherwise return default.
     return int(match.group()) if match is not None else default
 
 
+def get_int_or_float(val):
+    """Function to return `int` or `float` if the given string is one of the two.
+
+    Usage:
+    >>> get_int_or_float("123.45")
+    123.45
+    >>> get_int_or_float("130.0")
+    130
+    >>> get_int_or_float("130")
+    130"""
+
+    match = intfloat_pattern.search(val)
+    if match is not None:
+        if (match.group(2) is None) or (int(match.group(2)) == 0):
+            return int(match.group(0))
+        else:
+            return float(match.group())
+    else:
+        return None
+        # return int(match.group(0)) if match.group(2) is None else float(match.group())
+
+
+class DecimalBuilder:
+    """Class used to build a number, either an integer or a float."""
+
+    # Possibly use `type` instead of `has_decimal` and store type, ie `int` or `float`; will require validation though.
+    __slots__ = ("_value", "_has_decimal")
+
+    def __init__(self, value: str = "", has_decimal: bool = False):
+        self._value = value
+        self._has_decimal = has_decimal
+
+    @property
+    def value(self):
+        if not self._value:
+            return 0
+        return float(self._value) if self._has_decimal else int(self._value)
+
+    def set_val(self, value):
+        if type(value) == str:
+            val = get_int_or_float(value)
+            if val is None:
+                if not value:
+                    self._value = ""
+                    self._has_decimal = False
+                return
+        else:
+            val = value
+
+        val = round(val) if round(val) == val else val
+
+        if type(val) == int:
+            self._value = str(val)
+            self._has_decimal = False
+        elif type(val) == float:
+            self._value = str(val)
+            self._has_decimal = True
+
+    def get_string_val(self) -> str:
+        return self._value
+
+    def clear(self):
+        self.set_val("")
+
+    def append(self, value):
+        val = str(value)
+
+        if (get_int_or_float(val) is None) and (value != "."):
+            raise ValueError("'val' is not of a string or numeric type.")
+
+        if val ==".":
+            if self._has_decimal:
+                return
+            else:
+                self._has_decimal = True
+
+        self._value += val
+
+
 class Calculator(tk.Tk):
     """Class that manages the UI of the Calculator, it inherits from tkinter's Tk()."""
 
@@ -34,11 +119,11 @@ class Calculator(tk.Tk):
             self.title(title)
 
         # Display output vars as StringVar().
+        self.current = DecimalBuilder()
         self.display_current = StringVar()
         self.display_history = StringVar()
 
         # Calculator values.
-        self.current = 0
         self.total = 0
         self.total_prev = 0
         self.history = []
@@ -60,23 +145,28 @@ class Calculator(tk.Tk):
 
         # Main Frame
         self.widgets["main_frame"] = ttk.Frame(self)
-        self.widgets["main_frame"].grid(row=0, column=0, sticky=(tk.N, tk.E, tk.S, tk.W))
+        self.widgets["main_frame"].grid(
+            row=0, column=0, sticky=(tk.N, tk.E, tk.S, tk.W))
 
         # Label to display current equation, linked to `self.display_history`.
         self.widgets["output_history_label"] = ttk.Label(self.widgets["main_frame"], justify=tk.RIGHT,
                                                          textvariable=self.display_history)
-        self.widgets["output_history_label"].grid(row=0, column=0, columnspan=4, sticky=tk.W)
+        self.widgets["output_history_label"].grid(
+            row=0, column=0, columnspan=4, sticky=tk.W)
 
         # Entry to display current value, linked to `self.display_current`.
         self.widgets["output_current_entry"] = ttk.Entry(self.widgets["main_frame"], justify=tk.RIGHT,
                                                          textvariable=self.display_current, state="readonly")
-        self.widgets["output_current_entry"].grid(row=1, column=0, columnspan=4, sticky=(tk.N, tk.E, tk.S, tk.W))
+        self.widgets["output_current_entry"].grid(
+            row=1, column=0, columnspan=4, sticky=(tk.N, tk.E, tk.S, tk.W))
         # Binding the key and return event handler to handle keyboard key presses.
         self.widgets["output_current_entry"].bind("<Key>", self.key_click)
-        self.widgets["output_current_entry"].bind("Return", self.return_click())
+        self.widgets["output_current_entry"].bind(
+            "Return", self.return_click())
 
         # ScrolledText used to output calculation history.
-        self.widgets["output_history"] = st.ScrolledText(self.widgets["main_frame"], width=23, height=8)
+        self.widgets["output_history"] = st.ScrolledText(
+            self.widgets["main_frame"], width=23, height=8)
         self.widgets["output_history"].grid(row=0, column=5, rowspan=5)
 
         # The frame the contains all of the button inputs.
@@ -113,18 +203,33 @@ class Calculator(tk.Tk):
 
     def number_select(self, value):
         """Event handling method for handling numeric button clicks, ie buttons 0 through 9."""
+        if type(value) is int:
+            value = str(value)
+        elif type(value) is str:
+            val = get_int_or_float(value)
+            if val is not None:
+                value = val
+            else:
+                if value == ".":
+                    val = "."
+                else:
+                    return
+        else:
+            return
+
         self.showing_total = False
-        self.current = int(str(self.current) + str(value))
-        self.display_current.set(self.current)
+
+        self.current.append(value)
+        self.display_current.set(round(self.current.value, 3))
 
         # Set focus to the `output_current_entry` widget, for handling keyboard inputs.
         self.widgets["output_current_entry"].focus_set()
 
     def operator_select(self, operator):
         """Event handling method handling operator button clicks, ie ('+', '-', '*', '/')"""
-        if operator in ('+', '-') and self.current == 0:
+        if operator in ('+', '-') and self.current.value == 0:
             return
-        elif operator in ('*') and self.current == 1:
+        elif operator in ('*', "/") and self.current.value == 1:
             return
 
         # Temporarily store total in a local variable
@@ -133,29 +238,29 @@ class Calculator(tk.Tk):
 
         if operator == "=":
             # If the operator is '=', no need to perform calculations.
-            self.history.append(str(self.current))
-            self.current = self.total
+            self.history.append(self.current.get_string_val())
+            self.current.set_val(self.total)
             self.showing_total = True
             self.update_display()
         else:
             # Append the current value and then the operator to the history.
-            self.history.append(str(self.current))
+            self.history.append(self.current.get_string_val())
             self.history.append(operator)
 
             # Update the local `total` to match the operator
             if operator == "+":
-                total += self.current
+                total += self.current.value
             elif operator == "-":
-                total -= self.current
+                total -= self.current.value
             elif operator == "*":
-                total *= self.current
+                total *= self.current.value
             elif operator == "/'":
-                total /= self.current
+                total /= self.current.value
 
             # Display the current calculation in `output_history` widget.
-            self.widgets["output_history"].insert(tk.END, (str(self.total) + " " + operator + " " + str(self.current) +
+            self.widgets["output_history"].insert(tk.END, (str(self.total) + " " + operator + " " + self.current.get_string_val() +
                                                            " = " + str(total)) + "\n")
-            self.current = 0
+            self.current.clear()
             self.showing_total = True
             self.update_display(total)
         self.total = total
@@ -176,13 +281,16 @@ class Calculator(tk.Tk):
             self.number_select(val)
         else:
             # If the entered key is an operator, then call `self.operator_select()` with the inputted operator.
-            if event.char in ("+", "-", "*", "="):
+            if event.char == ".":
+                self.number_select(event.char)
+            elif event.char in ("+", "-", "*", "/", "="):
                 self.operator_select(event.char)
 
     def update_display(self, new_total=0):
         """Method to  update the data being displayed to the user by setting `self.display_current` and
         `self.display_history` StringVar() which are linked to widgets defined in `create_widgets()`."""
-        self.display_current.set(new_total if self.showing_total else str(self.current))
+        self.display_current.set(
+            new_total if self.showing_total else self.current.get_string_val())
         self.display_history.set(self.get_history_output())
 
     def get_history_output(self):
